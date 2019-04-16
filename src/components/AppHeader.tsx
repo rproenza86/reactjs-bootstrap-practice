cd041427a6efc276d2ae1bde98e745692d1d5411
Raúl Rodríguez Proenza <raul.r.proenza@gmail.com>
2019-04-16 19:41:41 -0400

    feat: Drawer menu functionality
    * Added logic to the AppHeader component to make it open the navigation menu when clicked the hamburger icon menu.
    
    Resolves: #2

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,34 +1,86 @@
 import * as React from 'react';
 
-class AppHeader extends React.Component {
+interface IAppHeaderState {
+    isMenuDrawerOpen: boolean;
+}
+
+class AppHeader extends React.Component<any, IAppHeaderState> {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            isMenuDrawerOpen: false
+        } as IAppHeaderState;
+    }
+
     public render() {
         return (
             <header>
                 <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-                    <a className="navbar-brand" href="#">Carousel</a>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                    <a className="navbar-brand" href="#">
+                        Carousel
+                    </a>
+                    <button
+                        className={
+                            'navbar-toggler' + (this.state.isMenuDrawerOpen ? ' collapsed' : '')
+                        }
+                        type="button"
+                        data-toggle="collapse"
+                        data-target="#navbarCollapse"
+                        aria-controls="navbarCollapse"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                        // tslint:disable-next-line: jsx-no-lambda
+                        onClick={() => this._toggleMenuDrawer()}
+                    >
                         <span className="navbar-toggler-icon" />
                     </button>
-                    <div className="collapse navbar-collapse" id="navbarCollapse">
+                    <div className={this._getNavbarClasses()} id="navbarCollapse">
                         <ul className="navbar-nav mr-auto">
                             <li className="nav-item active">
-                                <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                                <a className="nav-link" href="#">
+                                    Home <span className="sr-only">(current)</span>
+                                </a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">Link</a>
+                                <a className="nav-link" href="#">
+                                    Link
+                                </a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link disabled" href="#">Disabled</a>
+                                <a className="nav-link disabled" href="#">
+                                    Disabled
+                                </a>
                             </li>
                         </ul>
                         <form className="form-inline mt-2 mt-md-0">
-                            <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" />
-                            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                            <input
+                                className="form-control mr-sm-2"
+                                type="text"
+                                placeholder="Search"
+                                aria-label="Search"
+                            />
+                            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">
+                                Search
+                            </button>
                         </form>
                     </div>
                 </nav>
             </header>
         );
+    }
+
+    private _getNavbarClasses() {
+        let basicClassNames = 'collapse navbar-collapse';
+
+        if (this.state.isMenuDrawerOpen) {
+            basicClassNames += ' show';
+        }
+
+        return basicClassNames;
+    }
+
+    private _toggleMenuDrawer() {
+        this.setState({ isMenuDrawerOpen: !this.state.isMenuDrawerOpen });
     }
 }
 
