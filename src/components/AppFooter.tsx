6a0d8e7ec53117e495144cc09b229976ddf9306e
Raúl Rodríguez Proenza <raul.r.proenza@gmail.com>
2019-04-17 10:59:34 -0400

    feat: Convert class component
    * Converted class components into functionals components.
    
    Resolves: #11

--- src/components/AppFooter.tsx
+++ src/components/AppFooter.tsx
@@ -1,14 +1,15 @@
 import * as React from 'react';
 
-class AppFooter extends React.Component {
-    public render() {
-        return (
-            <footer className="container">
-                <p className="float-right"><a href="#">Back to top</a></p>
-                <p>&copy; 2019-2029 Proenza, Raul Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
-            </footer>
-        );
-    }
-}
+const AppFooter = () => (
+    <footer className="container">
+        <p className="float-right">
+            <a href="#">Back to top</a>
+        </p>
+        <p>
+            &copy; 2019-2029 Proenza, Raul Inc. &middot; <a href="#">Privacy</a> &middot;{' '}
+            <a href="#">Terms</a>
+        </p>
+    </footer>
+);
 
 export default AppFooter;
